018cf933cda3f158edd873308810620485b78025
Pierre PÉRONNET <pierre.peronnet@gmail.com>
2025-06-28 22:30:11 +0200

    feat(scripts) Introduce backup script
    Signed-off-by: Pierre PÉRONNET <pierre.peronnet@gmail.com>

---
+++ scripts/backup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+project_name="${1:-natbienetre}"
+CONTAINER_CLI="${CONTAINER_CLI:-podman}"
+
+echo "Starting containers..."
+
+"${CONTAINER_CLI}-compose" --project-name "$project_name" up --build --pull --quiet-pull --force-recreate --abort-on-container-failure --always-recreate-deps --remove-orphans sauvegarde
