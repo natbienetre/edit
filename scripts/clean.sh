d1c4c62efcf802c298c95095670fbeac98eccdb6
Pierre PÉRONNET <pierre.peronnet@gmail.com>
2025-06-30 12:15:47 +0200

    squash! feat(scripts) Split clean script in 2 parts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,4 +5,8 @@ set -euo pipefail
 project_name="${1:-natbienetre}"
 CONTAINER_CLI="${CONTAINER_CLI:-podman}"
 
-"${CONTAINER_CLI}-compose" --project-name "$project_name" down --rmi all --remove-orphans
+"${CONTAINER_CLI}-compose" \
+    --project-name "$project_name" \
+    down \
+        --rmi all \
+        --remove-orphans
