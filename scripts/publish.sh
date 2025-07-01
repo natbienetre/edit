a48bc4711bd95471bf1f7f2a6c17a8d147b51aaa
Pierre PÉRONNET <pierre.peronnet@gmail.com>
2025-07-01 22:50:33 +0200

    feat(scripts) Introduce publish.sh script
    Signed-off-by: Pierre PÉRONNET <pierre.peronnet@gmail.com>

---
+++ scripts/publish.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+project_name="${1:-natbienetre}"
+CONTAINER_CLI="${CONTAINER_CLI:-podman}"
+PODMAN_COMPOSE_PROVIDER="${PODMAN_COMPOSE_PROVIDER:-${CONTAINER_CLI}-compose}"
+
+echo "Starting containers..."
+
+"${PODMAN_COMPOSE_PROVIDER}" \
+    --project-name "$project_name" \
+    up \
+        --build \
+        --pull \
+        --quiet-pull \
+        --force-recreate \
+        --abort-on-container-failure \
+        --remove-orphans \
+        media \
+        publier
